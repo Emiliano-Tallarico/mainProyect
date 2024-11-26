9fc4285c255319ba32936a2516da92ce48e84f9b
Emiliano-Tallarico <02emilianotallarico@gmail.com>
2024-11-26 17:51:28 -0300

    Create main.py
    Sistema de batalla y pruebas generales.

---
+++ parcial/main.py
@@ -0,0 +1,11 @@
+
+
+
+def sistema_de_batalla(personajes):
+    cola = MaxHeap()
+
+for personaje in personajes:
+        max_heap.insert(personaje)
+    for enemigo in enemigos:
+        max_heap.insert(enemigo)
+
